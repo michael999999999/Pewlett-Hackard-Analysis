fa26c6e25dc9ed2d43f1b76630a24754b67a7eec
michael999999999 <100736206+michael999999999@users.noreply.github.com>
2022-05-05 19:57:10 -0400

    2205051957

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,7 +1,8 @@
 -- Module 7 Challenge
 
+-- CREATE RETIREMENT TITLES TABLE AND EXPORT
 SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
--- INTO retirement_titles
+INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
@@ -9,17 +10,28 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no;
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (emp_no) emp_no,
-first_name,
-last_name,
-title
+-- CREATE UNIQUE TITLES TABLE AND EXPORT
+SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
 INTO unique_titles
 FROM retirement_titles
 WHERE (to_date = '9999-01-01')
 ORDER BY emp_no, to_date DESC;
 
+--CREATE RETIRING TITLES TABLE AND EXPORT
 SELECT COUNT(title) title_count, title
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY title_count DESC;
+
+-- CREATE MENTORSHIP ELIGIBILITY TABLE AND EXPORT
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (de.emp_no = t.emp_no)
+WHERE (t.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
